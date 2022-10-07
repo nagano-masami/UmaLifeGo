fdfef7382130c3797c10c91734e3e2eb02838f6f
nagano.masami <nagano.masami.biz@gmail.com>
2022-10-07 11:18:32 +0900

    img etc change

--- frontend/src/components/parts/TopPage.vue
+++ frontend/src/components/parts/TopPage.vue
@@ -2,12 +2,9 @@
   <v-container>
     <v-row class="text-center" style="height:80vh">
       <v-col cols="12">
-        <v-img v-bind:src="require('../../../public/img/t1.png')"></v-img>
-        <v-img v-bind:src="require('../../../public/img/t2.png')"></v-img>
-        <v-img v-bind:src="require('../../../public/img/t3.png')"></v-img>
-        <v-img v-bind:src="require('../../../public/img/t4.png')"></v-img>
-        <v-img v-bind:src="require('../../../public/img/t5.png')"></v-img>
-        <v-img v-bind:src="require('../../../public/img/t6.png')"></v-img>
+        <v-img v-bind:src="require('../../../public/img/1.png')"></v-img>
+        <v-img v-bind:src="require('../../../public/img/2.png')"></v-img>
+        <v-img v-bind:src="require('../../../public/img/3.png')"></v-img>
         <v-toolbar color="#BBDEFB" light>
           <v-toolbar-title>Top Page</v-toolbar-title>
           <v-spacer></v-spacer>
@@ -21,6 +18,8 @@
         </v-toolbar>
         <!-- v-card内をスクロールさせるためにclass="overflow-y-auto"を指定 -->
         <v-card height="100%" width="100%" class="overflow-y-auto">
+        <v-img v-bind:src="require('../../../public/img/4.png')"></v-img>
+        <v-img v-bind:src="require('../../../public/img/5.png')"></v-img>
         </v-card>
       </v-col>
     </v-row>
