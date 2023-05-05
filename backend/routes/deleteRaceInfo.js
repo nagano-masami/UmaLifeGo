adf0cbcb8beb4781f3752ef30b8b1fbc804b88d6
kokoro <105964266+shinnyoro@users.noreply.github.com>
2023-05-05 18:46:51 +0900

    SG-207 <サーバーsql失敗時のロールバック処理>

--- backend/routes/deleteRaceInfo.js
+++ backend/routes/deleteRaceInfo.js
@@ -3,25 +3,29 @@ var router = express.Router();
 
 var mysql = require('mysql'); //★追加
 const config  = require('../public/javascripts/db_config.js');
+const DatabaseConnection = require('../public/javascripts/DatabaseConnection.js');
 
-// 削除フォームの送信処理
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
 
   const betting_ticket_id = req.body.betting_ticket_id;
   const id = req.body.id;
-  // データベースの設定情報
-  var connection = mysql.createConnection(config.mysql_setting);
+  const connection = new DatabaseConnection();
 
-  // データを取り出す
-  connection.query(config.deleteRaceInfoSQL,[id,betting_ticket_id] ,function (error, results, fields) {
-    // errorがnull、つまり成功した場合はOKを返す
-    if (!error) {
-      res.send('OK');
-    } else {
-      res.send('NG');
-    }
+  try {
+    await connection.connect(config.mysql_setting);
+    await connection.beginTransaction();
+    await connection.query(config.deleteRaceInfoSQL,[id,betting_ticket_id]);
+    await connection.commit();
+    res.send('OK');
+  } catch (error) {
+    console.error(error);
+    await connection.rollback();
+    res.send('NG');
+  } finally {
+    connection.end();
   }
-  );
 });
 
+
 module.exports = router;
+
--- backend/routes/saveRaceInfo.js
+++ backend/routes/saveRaceInfo.js
@@ -3,64 +3,74 @@ var router = express.Router();
 
 var mysql = require('mysql'); //★追加
 const config = require('../public/javascripts/db_config.js');
+const DatabaseConnection = require('../public/javascripts/DatabaseConnection.js');
 
-// 削除フォームの送信処理
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
 
-    var connection = mysql.createConnection(config.mysql_setting);
     raceInfo = req.body;
+    const connection = new DatabaseConnection();
 
-    res.send('OK');
-    // if (raceInfo.betting_ticket_id) {
-    //     // データを取り出す
-    //     connection.query(config.updateRaceInfoSQL,
-    //         [raceInfo.id,
-    //         raceInfo.bettingTicketId,
-    //         raceInfo.defaultBar.selectDate,
-    //         raceInfo.defaultBar.selectPlace,
-    //         raceInfo.defaultBar.selectRace_no,
-    //         raceInfo.InvestmentAmount,
-    //         raceInfo.balance,
-    //         raceInfo.horceSelection1,
-    //         raceInfo.horceSelection2,
-    //         raceInfo.horceSelection3,
-    //         raceInfo.selectFormula,
-    //         raceInfo.selectAmount,
-    //         raceInfo.selectUnitText,
-    //         raceInfo.selectUnitDigit], function (error, results, fields) {
-    //             // errorがnull、つまり成功した場合はOKを返す
-    //             if (!error) {
-    //                 res.send('OK');
-    //             } else {
-    //                 res.send('NG');
-    //             }
-    //         }
-    //     );
-    // } else {
-    //     // データを取り出す
-    //     connection.query(config.insertRaceInfoSQL,
-    //         [raceInfo.id,
-    //         raceInfo.defaultBar.selectDate,
-    //         raceInfo.defaultBar.selectPlace,
-    //         raceInfo.defaultBar.selectRace_no,
-    //         raceInfo.InvestmentAmount,
-    //         raceInfo.balance,
-    //         raceInfo.horceSelection1,
-    //         raceInfo.horceSelection2,
-    //         raceInfo.horceSelection3,
-    //         raceInfo.selectFormula,
-    //         raceInfo.selectAmount,
-    //         raceInfo.selectUnitText,
-    //         raceInfo.selectUnitDigit], function (error, results, fields) {
-    //             // errorがnull、つまり成功した場合はOKを返す
-    //             if (!error) {
-    //                 res.send('OK');
-    //             } else {
-    //                 res.send('NG');
-    //             }
-    //         }
-    //     );
-    // }
+    if (raceInfo.betting_ticket_id) {
+
+        try {
+            await connection.connect(config.mysql_setting);
+            await connection.beginTransaction();
+            await connection.query(config.updateRaceInfoSQL,
+                [raceInfo.id,
+                raceInfo.bettingTicketId,
+                raceInfo.defaultBar.selectDate,
+                raceInfo.defaultBar.selectPlace,
+                raceInfo.defaultBar.selectRace_no,
+                raceInfo.InvestmentAmount,
+                raceInfo.balance,
+                raceInfo.horceSelection1,
+                raceInfo.horceSelection2,
+                raceInfo.horceSelection3,
+                raceInfo.selectFormula,
+                raceInfo.selectAmount,
+                raceInfo.selectUnitText,
+                raceInfo.selectUnitDigit]);
+            await connection.commit();
+            res.send('OK');
+        } catch (error) {
+            console.error(error);
+            await connection.rollback();
+            res.send('NG');
+        } finally {
+            connection.end();
+        }
+
+    } else {
+        try {
+            await connection.connect(config.mysql_setting);
+            await connection.beginTransaction();
+            await connection.query(config.insertRaceInfoSQL,
+                [raceInfo.id,
+                raceInfo.defaultBar.selectDate,
+                raceInfo.defaultBar.selectPlace,
+                raceInfo.defaultBar.selectRace_no,
+                raceInfo.InvestmentAmount,
+                raceInfo.balance,
+                raceInfo.horceSelection1,
+                raceInfo.horceSelection2,
+                raceInfo.horceSelection3,
+                raceInfo.selectFormula,
+                raceInfo.selectAmount,
+                raceInfo.selectUnitText,
+                raceInfo.selectUnitDigit]);
+            await connection.commit();
+            res.send('OK');
+        } catch (error) {
+            console.error(error);
+            await connection.rollback();
+            res.send('NG');
+        } finally {
+            connection.end();
+        }
+    }
+
 });
 
+
 module.exports = router;
+
